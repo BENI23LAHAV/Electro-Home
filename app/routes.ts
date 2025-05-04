200b62477f57e833cc5e7c18079706b847b7ca4e
BENI23LAHAV <beni0548472300@gmail.com>
2025-05-04 13:15:23 +0300

    Some TW fixing

--- app/routes.ts
+++ app/routes.ts
@@ -2,8 +2,8 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
   layout("routes/wrapper.tsx", [
-    route("/:category?", "routes/home.tsx"),
-    route("/product/:id/:message?/:success?", "routes/singlProduct.tsx"),
+    route("/", "routes/home.tsx"),
+    route("/product/:id/", "routes/singlProduct.tsx"),
     route("/cart", "routes/shoppingCart.tsx"),
   ]),
 ] satisfies RouteConfig;
